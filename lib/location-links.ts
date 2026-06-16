d3f7d6ae0d00c5499e955e5ade5c815b0269301b
jasonbaroma <jasonbaroma@gmail.com>
2026-06-16 18:03:29 +0800

    Generate site content for compactcarhireblackpool

---
+++ lib/location-links.ts
@@ -0,0 +1,44 @@
+﻿import { slugifyLocation } from "@/lib/utils"
+
+export const mainLocationLink = {
+  label: "Blackpool",
+  href: "/",
+}
+
+const LOCATION_NAMES = [
+  "Lytham St Annes",
+  "Fleetwood",
+  "Poulton-le-Fylde",
+  "Thornton-Cleveleys",
+  "Preston",
+  "Southport",
+  "Lancaster",
+  "Morecambe",
+] as const
+
+export const nearbyLocationLinks = LOCATION_NAMES.map((label, index) => ({
+  index: index + 1,
+  label,
+  href: `/${slugifyLocation(label)}`,
+}))
+
+export function buildLocationLinks(currentLocationLabel?: string) {
+  return [mainLocationLink, ...getNearbyLocationLinks(currentLocationLabel)]
+}
+
+export function getNearbyLocationLinks(currentLocationLabel?: string) {
+  const currentSlug = currentLocationLabel
+    ? slugifyLocation(currentLocationLabel)
+    : ""
+
+  return nearbyLocationLinks.filter(
+    (location) => location.href !== `/${currentSlug}`,
+  )
+}
+
+export const locationSlugEntries = LOCATION_NAMES.map((label, index) => ({
+  index: index + 1,
+  label,
+  slug: slugifyLocation(label),
+}))
+
